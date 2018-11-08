85bcca7cc485740adeee80006257fee2a7784e4e
Caleb <mcaleb808@gmail.com>
2018-11-08 21:47:47 +0200

    changing api directory

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('hello world');
+import parcelRoutes from './routes/parcels';
+import userRoutes from './routes/users';
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
 });
 
-app.get('/api/orders', (req, res) => {
-	res.send([1,2,3]);
+app.use("/parcels", parcelRoutes);
+app.use("/users", userRoutes);
+
+app.use((req, res, next) => {
+  let error = new Error("Not found");
+  error.status = 404;
+  next(error);
 });
 
-app.listen(3000, () => console.log('listening on port 3000'));
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
+
+
