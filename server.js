b29e3ecf7bbde6a58b9e0532ad6c23a1119c57e2
Caleb <mcaleb808@gmail.com>
2018-11-09 17:31:07 +0200

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import http from 'http';
 import app from './app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log("App is running on port " + port);
+});
